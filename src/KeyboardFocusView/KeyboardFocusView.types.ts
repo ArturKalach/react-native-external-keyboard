5f0890d270b413e23a40ea5c554cf2702c54305e
Artur Kalach <artur_kalach@epam.com>
2023-07-17 22:20:43 +0200

    fix: types

--- src/KeyboardFocusView/KeyboardFocusView.types.ts
+++ src/KeyboardFocusView/KeyboardFocusView.types.ts
@@ -21,8 +21,8 @@ export type OnFocusChangeFn = (e: KeyboardFocusEvent) => void;
 
 export type FocusWrapperProps = ViewProps & {
   onFocusChange?: OnFocusChangeFn;
-  onKeyUpPress: OnKeyPressFn;
-  onKeyDownPress: OnKeyPressFn;
+  onKeyUpPress?: OnKeyPressFn;
+  onKeyDownPress?: OnKeyPressFn;
   canBeFocused?: boolean;
 };
 
