97c856461006b92740e0281914b13c4c40faf275
ArturKalach <artur.kalach.andreevich@gmail.com>
2024-05-19 22:19:10 +0200

    fix: fix lint issues

--- src/components/KeyboardFocusView/KeyboardFocusView.ts
+++ src/components/KeyboardFocusView/KeyboardFocusView.ts
@@ -2,6 +2,7 @@ import { View } from 'react-native';
 
 import type { KeyboardFocusViewProps } from '../../types';
 
-export const KeyboardFocusView = View as unknown as React.ForwardRefExoticComponent<
-  KeyboardFocusViewProps & React.RefAttributes<View>
->;
+export const KeyboardFocusView =
+  View as unknown as React.ForwardRefExoticComponent<
+    KeyboardFocusViewProps & React.RefAttributes<View>
+  >;
