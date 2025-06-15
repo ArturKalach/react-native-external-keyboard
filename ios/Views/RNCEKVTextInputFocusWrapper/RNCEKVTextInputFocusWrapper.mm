0165bf9837f4db8321e755c97753cf5ac3248b78
Artur Kalach <artur.kalach.andreevich@gmail.com>
2025-06-15 14:47:12 +0200

    fix: update on submit editing

--- ios/Views/RNCEKVTextInputFocusWrapper/RNCEKVTextInputFocusWrapper.mm
+++ ios/Views/RNCEKVTextInputFocusWrapper/RNCEKVTextInputFocusWrapper.mm
@@ -299,17 +299,18 @@ Class<RCTComponentViewProtocol> TextInputFocusWrapperCls(void)
         UIKey *key = presses.allObjects[0].key;
         BOOL isEnter = [key.characters isEqualToString:@"\n"] || [key.characters isEqualToString:@"\r"];
       
-        if(isEnter && _textField && !_textField.isFirstResponder) {
+        RCTUITextField* textView = _textField != nil ? _textField : [self getTextFieldComponent];
+        if(isEnter && textView && !textView.isFirstResponder) {
             [_textField reactFocus];
+            return;
         }
       
         if(self.multiline) {
             BOOL isShiftPressed = (key.modifierFlags & UIKeyModifierShift) != 0;
             
-            UIView* textView = _textField;
-            if(textView && _textField.isFirstResponder) {
+            if(textView && textView.isFirstResponder) {
                 if(!isShiftPressed && isEnter) {
-                    [self onMultiplyTextSubmitHandler: textView];
+                    [self onMultiplyTextSubmitHandler: (UIView*)textView];
                     if(self.blurOnSubmit) {
                         [textView resignFirstResponder];
                     }
