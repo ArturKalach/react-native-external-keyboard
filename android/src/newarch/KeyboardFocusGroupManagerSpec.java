c2e111a670b0064a3929f9579c21ad0e86aeef9f
Sebastian Weyrauch <github@dev-lab.de>
2025-12-05 17:22:06 +0100

    fix(android): Add missing getDelegate() for New Architecture ViewManagers

--- android/src/newarch/KeyboardFocusGroupManagerSpec.java
+++ android/src/newarch/KeyboardFocusGroupManagerSpec.java
@@ -1,8 +1,23 @@
 package com.externalkeyboard;
 
+import androidx.annotation.Nullable;
+
 import com.facebook.react.viewmanagers.KeyboardFocusGroupManagerInterface;
+import com.facebook.react.viewmanagers.KeyboardFocusGroupManagerDelegate;
+import com.facebook.react.uimanager.ViewManagerDelegate;
 import com.facebook.react.views.view.ReactViewGroup;
 import com.facebook.react.views.view.ReactViewManager;
 
 public abstract class KeyboardFocusGroupManagerSpec<T extends ReactViewGroup> extends ReactViewManager implements KeyboardFocusGroupManagerInterface<T> {
+  private final ViewManagerDelegate<T> mDelegate;
+
+  public KeyboardFocusGroupManagerSpec() {
+    mDelegate = new KeyboardFocusGroupManagerDelegate(this);
+  }
+
+  @Nullable
+  @Override
+  protected ViewManagerDelegate<T> getDelegate() {
+    return mDelegate;
+  }
 }
