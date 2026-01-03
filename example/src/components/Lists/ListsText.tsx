476040eea4ea904b1cb1da7346d85d9fb2dee464
Artur Kalach <artur.kalach.andreevich@gmail.com>
2026-01-03 16:30:08 +0100

    fix: update example

--- example/src/components/Lists/ListsText.tsx
+++ example/src/components/Lists/ListsText.tsx
@@ -1,25 +1,26 @@
-import { FlatList, Pressable, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, View } from 'react-native';
 
 const LIST_DATA = Array.from({ length: 10 });
 
 export const ListsTest = () => {
   return (
-    <View style={{ flex: 1 }}>
+    <View style={styles.container}>
       <FlatList
         data={LIST_DATA}
         onRefresh={() => {}}
         refreshing={false}
-        renderItem={() => (
-          <Pressable
-            style={{
-              borderColor: 'red',
-              borderWidth: 1,
-              width: '100%',
-              height: 50,
-            }}
-          />
-        )}
+        renderItem={() => <Pressable style={styles.pressable} />}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  pressable: {
+    borderColor: 'red',
+    borderWidth: 1,
+    width: '100%',
+    height: 50,
+  },
+});
