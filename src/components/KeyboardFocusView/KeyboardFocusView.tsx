780e13ed709175e772d3fe252d53a7fe0b4d41e7
Artur Kalach <artur.kalach.andreevich@gmail.com>
2026-04-24 16:47:03 +0200

    fix: update keyboard press handling with disabled state

--- src/components/KeyboardFocusView/KeyboardFocusView.tsx
+++ src/components/KeyboardFocusView/KeyboardFocusView.tsx
@@ -67,6 +67,7 @@ export const KeyboardFocusView = React.forwardRef<
       onPress,
       onLongPress,
       triggerCodes,
+      disabled: (props as { disabled?: boolean })?.disabled,
     });
 
     const HoverComonent = useMemo(() => {
