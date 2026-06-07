7201b17db31868f2fd86579f8a659052b36dffe7
Artur Kalach <artur.kalach.andreevich@gmail.com>
2026-06-07 22:10:26 +0200

    fix: update android events

--- android/src/main/java/com/externalkeyboard/events/EventHelper.java
+++ android/src/main/java/com/externalkeyboard/events/EventHelper.java
@@ -2,42 +2,39 @@ package com.externalkeyboard.events;
 
 import android.view.KeyEvent;
 
+import com.externalkeyboard.helper.ReactNativeEventDispatcher;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.uimanager.UIManagerHelper;
+import com.facebook.react.uimanager.events.Event;
 import com.facebook.react.uimanager.events.EventDispatcher;
 
 public class EventHelper {
   public static void focusChanged(ReactContext context, int id, boolean hasFocus) {
     int surfaceId = UIManagerHelper.getSurfaceId(context);
     FocusChangeEvent event = new FocusChangeEvent(surfaceId, id, hasFocus);
-    EventDispatcher eventDispatcher = UIManagerHelper.getEventDispatcherForReactTag(context, id);
-    if (eventDispatcher != null) {
-      eventDispatcher.dispatchEvent(event);
-    }
+    dispatch(context, id, event);
   }
 
   public static void pressDown(ReactContext context, int id, int keyCode, KeyEvent keyEvent) {
     int surfaceId = UIManagerHelper.getSurfaceId(context);
     KeyPressDownEvent keyPressDownEvent = new KeyPressDownEvent(surfaceId, id, keyCode, keyEvent);
-    EventDispatcher eventDispatcher = UIManagerHelper.getEventDispatcherForReactTag(context, id);
-    if (eventDispatcher != null) {
-      eventDispatcher.dispatchEvent(keyPressDownEvent);
-    }
+    dispatch(context, id, keyPressDownEvent);
   }
 
   public static void pressUp(ReactContext context, int id, int keyCode, KeyEvent keyEvent, boolean isLongPress) {
     int surfaceId = UIManagerHelper.getSurfaceId(context);
     KeyPressUpEvent keyPressUpEvent = new KeyPressUpEvent(surfaceId, id, keyCode, keyEvent, isLongPress);
-    EventDispatcher eventDispatcher = UIManagerHelper.getEventDispatcherForReactTag(context, id);
-    if (eventDispatcher != null) {
-      eventDispatcher.dispatchEvent(keyPressUpEvent);
-    }
+    dispatch(context, id, keyPressUpEvent);
   }
 
   public static void multiplyTextSubmit(ReactContext context, int id, String text) {
     int surfaceId = UIManagerHelper.getSurfaceId(context);
     MultiplyTextSubmit event = new MultiplyTextSubmit(surfaceId, id, text);
-    EventDispatcher eventDispatcher = UIManagerHelper.getEventDispatcherForReactTag(context, id);
+    dispatch(context, id, event);
+  }
+
+  private static void dispatch(ReactContext context, int id, Event<?> event) {
+    EventDispatcher eventDispatcher = ReactNativeEventDispatcher.getEventDispatcher(context, id);
     if (eventDispatcher != null) {
       eventDispatcher.dispatchEvent(event);
     }
